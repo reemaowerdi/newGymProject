e8736c1b5d1524c97643a483882a2302e73145cc
Lujain2001 <98377235+Lujain2001@users.noreply.github.com>
2022-04-29 05:18:40 +0300

    changed review

--- hh/9round-infoPage.php
+++ hh/9round-infoPage.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 
 <html lang="en">
 <head>
@@ -10,11 +10,16 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
     <!--for replay icon-->
-<a href="9RoundAdmin.php"></a>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!--for review -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <script>var swiper = new Swiper(".mySwiper", {
             slidesPerView: 3,
             spaceBetween: 30,
@@ -348,12 +353,6 @@
 
 
 
-
-
-
-
-
-
         @media (max-width:991px) {
             .home {
                 margin-top: 70px;
@@ -388,7 +387,7 @@
 </head>
 <body>
 
-    <!-------header ------>
+    <!---header ------>
     <header>
 
         <style>
@@ -487,7 +486,6 @@
                     <h2>overview</h2>
                     <h2>build your body &</h2>
                     <h1>shape <span>yourself</span> </h1>
-                            <!--هنا التغيير مع الصورة وموقع النادي-->
                     <p>
                         At typical gyms, your workout relies on a class time or your trainer’s schedule. We think it's about time that changed. We provide members with fun,
                         trainer-guided, kickboxing-style workouts in a circuit format. That means our workouts begin whenever you show up.
@@ -506,7 +504,7 @@
 
     <div class="our-class">
         <h1>classes</h1>
-           <!--هنا -->
+
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
@@ -551,7 +549,7 @@
         </div>
     </div>
 
-    <!-- our stories GOOGLE MAP هنا -->
+    <!-- our stories GOOGLE MAP-->
     <div class="our-story">
         <h1>gym location</h1>
         <div class="ourstory-main">
@@ -705,8 +703,83 @@
             margin-bottom: 20px;
         }
     </style>
+    <!--review-->
+<script src="script.js"></script>
+    <div class="container">
+  <h1 class="mt-5 mb-5">Reviews</h1>
+  <div class="card">
+    <div class="card-header">write and rate the gym</div>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-sm-4 text-center">
+          <h1 class="text-warning mt-4 mb-4">
+            <b><span id="average_rating">0.0</span> / 5</b>
+          </h1>
+          <div class="mb-3">
+            <i class="fas fa-star star-light mr-1 main_star"></i>
+                        <i class="fas fa-star star-light mr-1 main_star"></i>
+                        <i class="fas fa-star star-light mr-1 main_star"></i>
+                        <i class="fas fa-star star-light mr-1 main_star"></i>
+                        <i class="fas fa-star star-light mr-1 main_star"></i>
+          </div>
+          <h3><span id="total_review">0</span> Review</h3>
+        </div>
+        <div class="col-sm-4">
+          <p>
+                        <div class="progress-label-left"><b>5</b> <i class="fas fa-star text-warning"></i></div>
 
-    <div id="add-reviews">
+                        <div class="progress-label-right">(<span id="total_five_star_review">0</span>)</div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="five_star_progress"></div>
+                        </div>
+                    </p>
+          <p>
+                        <div class="progress-label-left"><b>4</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <div class="progress-label-right">(<span id="total_four_star_review">0</span>)</div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="four_star_progress"></div>
+                        </div>
+                    </p>
+          <p>
+                        <div class="progress-label-left"><b>3</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <div class="progress-label-right">(<span id="total_three_star_review">0</span>)</div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="three_star_progress"></div>
+                        </div>
+                    </p>
+          <p>
+                        <div class="progress-label-left"><b>2</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <div class="progress-label-right">(<span id="total_two_star_review">0</span>)</div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="two_star_progress"></div>
+                        </div>
+                    </p>
+          <p>
+                        <div class="progress-label-left"><b>1</b> <i class="fas fa-star text-warning"></i></div>
+
+                        <div class="progress-label-right">(<span id="total_one_star_review">0</span>)</div>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" id="one_star_progress"></div>
+                        </div>
+                    </p>
+        </div>
+        <div class="col-sm-4 text-center">
+          <h3 class="mt-4 mb-3">Write Review Here</h3>
+          <button type="button" name="add_review" id="add_review" class="btn btn-primary">Review</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mt-5" id="review_content"></div>
+</div>
+
+
+
+
+    <!--   <div id="add-reviews">
         <div class="col-md-3"></div>
 
         <div class="col-md-6 well">
@@ -742,9 +815,9 @@
         <script src="script.js"></script>
 
     </div>
-    <br>
+    <br>-->
     <!-- reviews -->
-    <div class="customer-review">
+    <!--<div class="customer-review">
         <h1>reviews</h1>
 
         <div class="main-customer">
@@ -790,7 +863,7 @@
 
             </div>
         </div>
-    </div>
+    </div>-->
 
 
 
@@ -799,29 +872,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>var swiper = new Swiper(".mySwiper", {
             slidesPerView: 3,
@@ -947,6 +997,10 @@
    font-weight: 200;
 }
 
+
+
+
+
 </style>
 
 <footer id="footer">
@@ -970,3 +1024,55 @@
 </footer>
 </body>
 </html>
+
+<!--new code review-->
+
+<div id="review_modal" class="modal" tabindex="-1" role="dialog">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+	      	<div class="modal-header">
+	        	<h5 class="modal-title">Submit Review</h5>
+	        	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          		<span aria-hidden="true">&times;</span>
+	        	</button>
+	      	</div>
+	      	<div class="modal-body">
+	      		<h4 class="text-center mt-2 mb-4">
+	        		      <i class="fas fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
+                    <i class="fas fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
+	        	</h4>
+	        	<div class="form-group">
+	        		<input type="text" name="user_name" id="user_name" class="form-control" placeholder="Enter Your Name" />
+	        	</div>
+	        	<div class="form-group">
+	        		<textarea name="user_review" id="user_review" class="form-control" placeholder="Type Review Here"></textarea>
+	        	</div>
+	        	<div class="form-group text-center mt-4">
+	        		<button type="button" class="btn btn-primary" id="save_review">Submit</button>
+	        	</div>
+	      	</div>
+    	</div>
+  	</div>
+</div>
+
+<style>
+.progress-label-left
+{
+    float: left;
+    margin-right: 0.5em;
+    line-height: 1em;
+}
+.progress-label-right
+{
+    float: right;
+    margin-left: 0.3em;
+    line-height: 1em;
+}
+.star-light
+{
+	color:#e9ecef;
+}
+</style>
